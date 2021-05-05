e7db65572f428f1ef2aaf3e9c67c06992b050deb
aavinokurov <aavinokurov@outlook.com>
2021-05-05 19:04:21 +0300

    Add send file

--- HomeWork9/HomeWork9/MyTelegramBot.cs
+++ HomeWork9/HomeWork9/MyTelegramBot.cs
@@ -22,6 +22,11 @@ namespace HomeWork9
         /// </summary>
         private string token;
 
+        /// <summary>
+        /// Режим работы бота
+        /// </summary>
+        private bool isDownload;
+
         #endregion
 
         #region Свойства
@@ -59,7 +64,7 @@ namespace HomeWork9
         /// </summary>
         /// <param name="fileId">Id файла</param>
         /// <param name="fileName">Имя файла</param>
-        public async void DownLoad(string fileId, string fileName)
+        public async void DownLoadFile(string fileId, string fileName)
         {
             var file = await bot.GetFileAsync(fileId);
             FileStream fs = new FileStream($@"{DownloadFolder}/{fileName}", FileMode.Create);
@@ -73,17 +78,136 @@ namespace HomeWork9
         /// </summary>
         /// <param name="chatId">ID чата</param>
         /// <param name="fileName">Имя файла</param>
-        public async void SendFile(ChatId chatId, string fileName)
+        public async void SendFile(ChatId chatId, string path)
         {
-            using (var fs = File.OpenRead($@"{DownloadFolder}/{fileName}"))
+            using (var fs = File.OpenRead(path))
             {
                 Telegram.Bot.Types.InputFiles.InputOnlineFile iof =
                     new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs);
 
-                iof.FileName = fileName;
-
-                var send = await bot.SendDocumentAsync(chatId, iof);
+                var send = await bot.SendVoiceAsync(chatId, iof);
             }
+        }
+
+        /// <summary>
+        /// Выбрать тип файл
+        /// </summary>
+        public void SelectTypeFile(Telegram.Bot.Args.MessageEventArgs messageEvent)
+        {
+            string sendMessage = "Выбери тип файла:\n" +
+                                 "1 - Документ\n" +
+                                 "2 - Аудиосообщение";
+
+            bot.SendTextMessageAsync(messageEvent.Message.Chat.Id, sendMessage);
+        }
+
+        public void SelectFile(Telegram.Bot.Args.MessageEventArgs messageEvent)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(DownloadFolder);
+            var namesFile = directoryInfo.GetFiles();
+            string messageFile;
+
+            if (namesFile.Length > 0)
+            {
+                messageFile = "Выбери файл для скачивания:\n";
+
+                for (int i = 0; i < namesFile.Length; i++)
+                {
+                    messageFile += $"{i + 1} - {namesFile[i].Name}\n";
+                }
+            }
+            else
+            {
+                messageFile = "Файлов для скачивания нет.";
+            }
+
+            bot.SendTextMessageAsync(messageEvent.Message.Chat.Id, messageFile);
+        }
+
+        /// <summary>
+        /// Реакция на новое сообщение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="messageEvent"></param>
+        public void TakeMessage(object sender, Telegram.Bot.Args.MessageEventArgs messageEvent)
+        {
+            PrintInfoMessage(messageEvent);
+
+            if (messageEvent.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                DownLoadFile(messageEvent.Message.Document.FileId, @"Document/" + messageEvent.Message.Document.FileName);
+            }
+
+            if (messageEvent.Message.Type == Telegram.Bot.Types.Enums.MessageType.Voice)
+            {
+                DownLoadFile(messageEvent.Message.Voice.FileId, @"Voice/" + DateTime.Now.ToString("yyyy.MM.dd HH_mm") + ".ogg");
+            }
+
+            if (isDownload)
+            {
+                int answer;
+
+                if (DownloadFolder == @"Download")
+                {
+                    if (Int32.TryParse(messageEvent.Message.Text, out answer) && answer > 0 && answer < 3)
+                    {
+                        switch (answer)
+                        {
+                            case 1:
+                                DownloadFolder += @"\Document";
+                                break;
+                            case 2:
+                                DownloadFolder += @"\Voice";
+                                break;
+                        }
+
+                        SelectFile(messageEvent);
+                    }
+                    else
+                    {
+                        bot.SendTextMessageAsync(messageEvent.Message.Chat.Id, "Такого типа файла нет.");
+                        isDownload = false;
+                    }
+                }
+                else
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(DownloadFolder);
+                    var files = directoryInfo.GetFiles();
+
+                    if (Int32.TryParse(messageEvent.Message.Text, out answer) && answer > 0 && answer <= files.Length)
+                    {      
+                        SendFile(messageEvent.Message.Chat.Id, files[answer - 1].FullName);
+                    }
+                    isDownload = false;
+                    DownloadFolder = @"Download";
+                }
+            }
+
+            if (messageEvent.Message.Text == @"/Download")
+            {
+                isDownload = true;
+                SelectTypeFile(messageEvent);
+            }
+
+            if (messageEvent.Message.Text == null) return;
+
+            var messageText = messageEvent.Message.Text;
+
+
+            bot.SendTextMessageAsync(messageEvent.Message.Chat.Id,
+                $"{messageText}"
+            );
+        }
+
+        /// <summary>
+        /// Вывод в консоль информацию о сообщении
+        /// </summary>
+        /// <param name="messageEvent"></param>
+        public void PrintInfoMessage(Telegram.Bot.Args.MessageEventArgs messageEvent)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {messageEvent.Message.Chat.FirstName} {messageEvent.Message.Chat.Id} {messageEvent.Message.Text}";
+
+            Console.WriteLine($"{text} TypeMessage: {messageEvent.Message.Type}");
         }
 
         #endregion
--- HomeWork9/HomeWork9/Program.cs
+++ HomeWork9/HomeWork9/Program.cs
@@ -19,70 +19,13 @@ namespace HomeWork9
 
         static void Main(string[] args)
         {
-            string token = File.ReadAllText(@"../../../../../token.txt");
+            string token = "";
 
-            bot = new TelegramBotClient(token);
-            bot.OnMessage += MessageListener;
-            bot.StartReceiving();
+            MyTelegramBot telegramBot = new MyTelegramBot(token);
+
+            telegramBot.bot.OnMessage += telegramBot.TakeMessage;
+            telegramBot.bot.StartReceiving();
             Console.ReadKey();
-        }
-        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
-        {
-            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-
-            Console.WriteLine($"{text} TypeMessage: {e.Message.Type}");
-
-            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
-            {
-                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
-            }
-
-            if (e.Message.Text == @"/Download")
-            {
-                DirectoryInfo directoryInfo = new DirectoryInfo(@"Download");
-                var namesFile = directoryInfo.GetFiles();
-
-                string messageFile = "Выбери файл для скачивания:\n";
-
-                for (int i = 0; i < namesFile.Length; i++)
-                {
-                    messageFile += $"{i + 1} - {namesFile[i].Name}\n";
-                }
-
-                bot.SendTextMessageAsync(e.Message.Chat.Id, messageFile);
-            }
-            
-            if (e.Message.Text == null) return;
-
-            var messageText = e.Message.Text;
-
-            
-            bot.SendTextMessageAsync(e.Message.Chat.Id,
-                $"{messageText}"
-            );
-        }
-        
-        static async void DownLoad(string fileId, string path)
-        {
-            var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
-            await bot.DownloadFileAsync(file.FilePath, fs);
-            fs.Close();
-             
-            fs.Dispose();
-        }
-
-        static async void SendFile(ChatId chatId, string path)
-        {
-            using (var fs = File.OpenRead(path))
-            {
-                Telegram.Bot.Types.InputFiles.InputOnlineFile iof =
-                    new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs);
-
-                iof.FileName = "Document.pdf";
-
-                var send = await bot.SendDocumentAsync(chatId, iof);
-            }
         }
     }
 }
